51732fabcffe596b65625184e8fc97a9dac129e1
Pho3nixG0D <oliverkrskagmail.com>
2022-12-02 00:21:47 +0100

    10.02 changed book.cs called obj in program

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -11,9 +11,14 @@ namespace Open_Lab_10._00
         static void Main(string[] args)
         {
             Book LOTR = new Book();
-            LOTR.title = "The Lord of the Rings";
-           // LOTR.Pages = 1178;
-            Console.WriteLine(LOTR.title);
+            ///LOTR.title = "The Lord of the Rings";
+            LOTR.SetTitle("The Lord of the Rings" );
+            LOTR.SetPages(1178);
+            LOTR.SetCategory("Heroic Romance");
+            LOTR.SetAuthor("John Ronald Reuel Tolkien");
+            LOTR.SetReleaseDate(1954);
+
+            LOTR.WriteAll();
         }
     }
 }
--- Open-Lab-10.00/book.cs
+++ Open-Lab-10.00/book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +9,64 @@ namespace Open_Lab_10._00
 {
     internal class Book
     {
-        public string title;
+        private string title;
         private int pages;
+        private string category;
+        private string author;
+        private int releaseDate;
         
-            
-
-        
 
 
+        public string GetTitle()
+        {
+            return this.title;
+        }
+        public string SetTitle(string title)
+        {
+            this.title = title;
+            return title;
+        }
+        public int GetPages(int pages)
+        {
+            return this.pages;
+        }
+        public int SetPages(int pages)
+        {
+            this.pages = pages;
+            return pages;
+        }
+        public string GetCategory(string category)
+        {
+            return this.category;
+        }
+        public string SetCategory(string category)
+        {
+            this.category = category;
+            return category;
+        }
+        public string GetAuthor(string author)
+        {
+            return this.author;
+        }
+        public string SetAuthor(string author)
+        {
+            this.author = author;
+            return author;
+        }
 
+        public int GetReleaseDate(int releaseDate)
+        {
+            return this.releaseDate;
+        }
+        public int SetReleaseDate(int releaseDate)
+        {
+            this.releaseDate = releaseDate;
+            return releaseDate;
+        }
+
+        public void WriteAll()
+        {
+            Console.Write($"{title}\n{pages}\n{category}\n{author}\n{releaseDate}\n");
+        }
     }
 }
