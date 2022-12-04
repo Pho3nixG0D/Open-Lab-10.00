aee500c0321e3d095944f246c1c93e7749d89fc9
Pho3nixG0D <oliverkrskagmail.com>
2022-12-04 01:34:51 +0100

    10.05 created list with categories, category is set by value from list

--- Open-Lab-10.00/Program.cs
+++ Open-Lab-10.00/Program.cs
@@ -20,11 +20,10 @@ namespace Open_Lab_10._00
             Book LOTR2 = new Book();
             Book LOTR3 = new Book( "Lord of The Rings 3" , 416);
             Book HOBIT = new Book("The Hobbit" , 304, "Fantasy novel", "John Ronald Reuel Tolkien", 1937);
+            HOBIT.Category = Book.categoryList[4];
 
-            Console.WriteLine(LOTR.ToString());
-            Console.WriteLine(LOTR2.ToString());
-            Console.WriteLine(LOTR3.ToString());
-            Console.WriteLine(HOBIT.ToString());
+            Console.WriteLine($"{LOTR}\n{LOTR2}\n{LOTR3}\n{HOBIT}\n");
+
         }
     }
 }
--- Open-Lab-10.00/book.cs
+++ Open-Lab-10.00/book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,8 +15,10 @@ namespace Open_Lab_10._00
         private string category;
         private string author;
         private int releaseDate;
-
-
+        /// <summary>
+        /// This is List of book categories.
+        /// </summary>
+        public static List<string> categoryList = new List<string>() { "detské", "romantické", "náučné", "sci-fi", "dobrodružné" };
         public Book()
         {
             title = "-1";
@@ -63,7 +66,6 @@ namespace Open_Lab_10._00
 
         public string Category
         {
-            get { return category; }
             set { category = value; }
         }
 
@@ -90,11 +92,11 @@ namespace Open_Lab_10._00
             }
         }
 
-         public override string ToString()
+        public override string ToString()
         {
             return String.Format($"{title}\n{pages}\n{category}\n{author}\n{releaseDate}\n");
         }
-        
+
         /*  public string GetTitle()
         {
         return this.title;
